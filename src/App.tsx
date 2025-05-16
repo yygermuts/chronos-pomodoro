717ba714925f543bd71a67df3001769e82b0d6de
Yuri Germuts <yygermuts@gmail.com>
2025-05-15 21:05:41 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/theme.css'
+import './styles/global.css'
+
+
+function App() {
+  return <h1 className='heading'>Hello World</h1>
+}
+    
+
+export default App
---
+++ src/components/Heading.tsx
@@ -0,0 +1,7 @@
+import '../styles/theme.css'
+import '../styles/global.css'
+import styles from './Heading.module.css'
+
+export function Heading () {
+    return <h1 className={styles.heading}>Hello World!</h1>
+}
